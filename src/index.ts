f01d20decfd856b9879e547778349edcc60264e6
Oleksandr <didyshen.oleksandr@gmail.com>
2023-03-18 18:55:15 +0200

    Add routing

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,33 @@
 import express from 'express';
-import { readFile } from 'fs';
+import { readFile } from 'fs/promises';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send(
-      readFile(
-        'asdlfkjlasdkjflkasdjflkajsdlfkjsadlkfjlaksdjf',
-        'utf-8',
-        () => {}
-      )
-    );
+app.get('/', async (req, res) => {
+  res.send(await readFile('public/index.html', 'utf8'));
+});
+
+app.get('/about', async (req, res) => {
+  res.send(await readFile('public/about.html', 'utf8'));
+});
+
+app.get('/contact', async (req, res) => {
+  res.status(200).send(await readFile('public/contact.html', 'utf8'));
+});
+
+app.get('/styles.css', async (req, res) => {
+  const css = await readFile('public/styles.css', 'utf8');
+  res.set('Content-Type', 'text/css');
+  res.send(css);
+});
+
+app.use(async function async(req, res, next) {
+  res.status(404).send(await readFile('public/404.html', 'utf8'));
 });
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Example app listening on port ${port}!`);
+  console.log(
+    `⚡️[server]: Example app listening on port http://localhost:${port}!`
+  );
 });
